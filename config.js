97ad9a9173c44ffe68c93b43c1de8c38ad7dcc14
JaniMano297 <129065716+JaniMano297@users.noreply.github.com>
2026-08-27 11:19:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,8 @@ global.botName = "DANGERS111 MD"
 global.owner = ["923190528626"]
 global.ownerName = "Dangers111 MD"
 global.prefix = "."
+
+// YAHAN APNE LINK DALO
+global.whatsappChannel = "https://whatsapp.com/channel/0029Vb8svknLdQeaawSSil1o"
+global.youtube = "https://youtube.com/@lucky-boy265"
+global.telegram = "https://t.me/Dangers111"
