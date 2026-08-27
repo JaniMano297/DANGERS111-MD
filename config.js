c9954b523c11baa0f9282214892f7ce252491a14
JaniMano297 <129065716+JaniMano297@users.noreply.github.com>
2026-08-27 14:55:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-global.botName = "DANGERS111 MD"
-global.owner = ["923190528626"]
-global.ownerName = "Dangers111 MD"
-global.prefix = "."
-
-// YAHAN APNE LINK DALO
-global.whatsappChannel = "https://whatsapp.com/channel/0029Vb8svknLdQeaawSSil1o"
-global.youtube = "https://youtube.com/@lucky-boy265"
-global.telegram = "https://t.me/Dangers111"
+global.prefix = '.'
+global.botName = 'DANGERS111 MD'
+global.ownerName = 'DANGERS'
+global.owner = ['923190528626'] // apna number
+global.whatsappChannel = 'https://whatsapp.com/channel/0029Vb8svknLdQeaawSSil1o'
+global.youtube = 'https://youtube.com/@lucky-boy265'
+global.telegram = 'https://t.me/Dangers111'
