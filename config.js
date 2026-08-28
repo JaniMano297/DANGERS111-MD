ffacdbbdf4a5bd31eb38d46c32a03dc51e14cc67
JaniMano297 <129065716+JaniMano297@users.noreply.github.com>
2026-08-28 11:59:07 +0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-global.prefix = '.'
-global.botName = 'DANGERS111 MD'
-global.ownerName = 'DANGERS'
-global.owner = ['923190528626'] // apna number
-global.whatsappChannel = 'https://whatsapp.com/channel/0029Vb8svknLdQeaawSSil1o'
-global.youtube = 'https://youtube.com/@lucky-boy265'
-global.telegram = 'https://t.me/Dangers111'
